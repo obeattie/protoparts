81c9bee43c768e407e9f249d67d93e789274405f
Oliver Beattie <oliver@obeattie.com>
2023-08-01 13:30:50 +0200

    Add fuzz test

---
+++ parts_fuzz_test.go
@@ -0,0 +1,70 @@
+package protoparts
+
+import (
+	"bytes"
+	"encoding/base64"
+	"os/exec"
+	"testing"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/dynamicpb"
+
+	ppproto "github.com/obeattie/protoparts/test/proto"
+	"github.com/stretchr/testify/assert"
+)
+
+func FuzzSplit(f *testing.F) {
+	msgs := []*dynamicpb.Message{
+		testMsg(f, nil, nil, nil, nil, nil, nil),
+		testMsg(f, s(""), nil, nil, nil, nil, nil),
+		testMsg(f, s("Oliver Beattie"), nil, nil, nil, nil, nil),
+		testMsg(f, s("Lindy Bishop"), nil, nil, nil, nil, nil),
+		testMsg(f,
+			s("Ryan Gosling"),
+			s("3532 Hayden Ave"),
+			s("Culver City"),
+			[]string{"The Driver", "Sebastian Wilder", ""},
+			[][]byte{[]byte("🕺"), []byte("🏍")},
+			map[string]string{
+				"k1": "v1",
+				"k2": "v2",
+				"k3": "v3",
+				"k4": "v4",
+			}),
+		testMsg(f,
+			s("Ryan Gosling"),
+			s("3532 Hayden Ave"),
+			s("Culver City"),
+			[]string{"The Driver", "Sebastian Wilder"},
+			[][]byte{[]byte("🕺"), []byte("🏍")},
+			map[string]string{
+				"k1": "v1",
+				"k2": "v2",
+				"k3": "v3",
+				"k4": "v4",
+			}),
+	}
+	for _, msg := range msgs {
+		f.Add(marshalProto(f, msg))
+	}
+
+	f.Fuzz(func(t *testing.T, b []byte) {
+		pb := &ppproto.Person{}
+		if err := proto.Unmarshal(b, pb); err != nil {
+			t.Skip()
+		}
+		md := pb.ProtoReflect().Descriptor()
+		_, err := Split(b, md)
+		if !assert.NoError(t, err) {
+			t.Logf("base64 protobuf: 		%s", base64.StdEncoding.EncodeToString(b))
+			t.Logf("parsed protobuf: 		%s", pb.String())
+
+			protocCmd := exec.Command("protoc", "--decode_raw")
+			protocCmd.Stdin = bytes.NewReader(b)
+			protocOutput, err := protocCmd.Output()
+			if err == nil {
+				t.Logf("protoc --decode_raw: 	%s", string(protocOutput))
+			}
+		}
+	})
+}
